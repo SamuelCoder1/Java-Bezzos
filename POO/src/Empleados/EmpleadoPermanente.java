0eac3eb22e9ac7a1072afda8cb941771c8d52b27
SamuelCoder1 <161090562+SamuelCoder1@users.noreply.github.com>
2024-07-25 16:08:30 -0500

    Delete POO directory

--- POO/src/Empleados/EmpleadoPermanente.java
+++
@@ -1,8 +0,0 @@
-package Empleados;
-
-public class EmpleadoPermanente extends Empleado{
-    @Override
-    public void irTrabajo(){
-        System.out.println("Hola, soy un empleado permanente");
-    }
-}
--- POO/src/Empleados/EmpleadoTemporal.java
+++
@@ -1,8 +0,0 @@
-package Empleados;
-
-public class EmpleadoTemporal extends Empleado{
-    @Override
-    public void irTrabajo(){
-        System.out.println("Hola, soy un empleado temporal");
-    }
-}
